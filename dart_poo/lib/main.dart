62a81aca195e7880297cbe55a153e0c793ad66d3
Murilo Silva <murilo.alex.silva@gmail.com>
2021-03-16 09:35:23 -0300

    Termino do curso

--- dart_poo/lib/main.dart
+++ dart_poo/lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:dart_poo/src/pessoa.dart';
+import 'dart:io';
+
+Pessoa pessoa = Pessoa();
 
 main(List<String> args) {
-  Pessoa pessoa = Pessoa(nome: 'Murilo', idade: 31, sexo: 'M');
-  print(pessoa.nome);
-  print(pessoa.idade);
-  print(pessoa.sexo);
+  print('Escreva seu nome: ');
+  pessoa.nome = stdin.readLineSync();
+  print('Escreva seu idade: ');
+  pessoa.idade = int.parse(stdin.readLineSync());
+  print('Escreva seu sexo: ');
+  pessoa.sexo = stdin.readLineSync();
+  print('Escreva seu peso: ');
+  pessoa.peso = double.parse(stdin.readLineSync());
+  print('Escreva seu altura: ');
+  pessoa.altura = double.parse(stdin.readLineSync());
+
+  double imc = pessoa.peso / (pessoa.altura * pessoa.altura);
+
+  print(
+      'Seu nome é ${pessoa.nome}, você tem ${pessoa.idade} anos, do sexo ${pessoa.sexo}, com o peso de ${pessoa.peso} Kgs, altura de ${pessoa.altura} e IMC de $imc');
+  print('\n');
 }
--- dart_poo/lib/src/pessoa.dart
+++ dart_poo/lib/src/pessoa.dart
@@ -5,5 +5,12 @@ class Pessoa extends Humano {
   int idade;
   String sexo;
 
-  Pessoa({this.nome, this.idade, this.sexo});
+  Pessoa({this.nome, this.idade, this.sexo, altura, peso})
+      : super(altura: altura, peso: peso);
+
+  /*
+        Para utilizar as var do extends, vc deverá colocar no construtor o nome das var e 
+        inserir o super com os parametros, assim vc conseguirá utilizar 
+        quando for chamar a classe Pessoa
+      */
 }
